8a23978f84bae726be3d5257945458dedd582385
Tom Allen <tomallen851@yahoo.com>
2021-03-01 17:10:20 -0600

    to kabab case

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,5 @@ export function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export const toKebabCase = (str) => str.toLowerCase().replace(" ", "-");
